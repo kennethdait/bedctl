e2b67b76cac8bf62e86d6ee195ab11af649a7bc5
Kenneth Dait <kendaitdev@icloud.com>
2021-02-19 16:36:26 -0600

    booklist.js: added skeleton structure comments

--- src/booklist.js
+++ src/booklist.js
@@ -1,6 +1,34 @@
 (function(){
   'use strict';
 
-  
+  /**
+   * Book Class
+   * Represents a book
+   */
+
+   
+
+   /**
+    * UI Class
+    * Handle UI tasks
+    */
+
+
+
+    /**
+     * Store Class
+     * Handles storage
+     */
+
+
+
+     // Event: Display books
+
+
+     // Event: Add a book
+
+
+     // Event: Remove a book
+
 
 }());
