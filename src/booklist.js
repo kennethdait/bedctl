8c34b3c7e44932c4421ad3682a5c844d9c5ef15b
Kenneth Dait <kendaitdev@icloud.com>
2021-02-19 16:57:01 -0600

    created Book class
    * has 3 properties:
      * title
      * author
      * ISBN number

--- src/booklist.js
+++ src/booklist.js
@@ -5,9 +5,13 @@
    * Book Class
    * Represents a book
    */
-
-   
-
+  class Book {
+    constructor(title,author,isbn) {
+      this.title = title;
+      this.author = author;
+      this.isbn = isbn;
+    }
+  }
    /**
     * UI Class
     * Handle UI tasks
