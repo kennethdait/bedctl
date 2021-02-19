4b9b1f18566040b4a5467f1561513bb76f955aaf
Kenneth Dait <kendaitdev@icloud.com>
2021-02-19 17:08:32 -0600

    added hard-coded data
    * for now, pulling stored books from hard-coded data
    * later should pull from local storage

--- src/booklist.js
+++ src/booklist.js
@@ -19,6 +19,35 @@
    class UI {
      static displayBooks() {
 
+      // TODO: simulating local storage (with hard-coded data)
+       const storedBooks = [
+         {
+           title: 'Book One',
+           author: 'John Doe',
+           isbn: '3434434'
+         },
+         {
+          title: 'Book Two',
+          author: 'Jane Doe',
+          isbn: '4552525'
+        },
+        {
+          title: 'Book Three',
+          author: 'Tony Stark',
+          isbn: '34433677'
+        },
+        {
+          title: 'Book Four',
+          author: 'Peter Parker',
+          isbn: '4327876'
+        },
+        {
+          title: 'Book Five',
+          author: 'Connick Dait',
+          isbn: '44767744'
+        },
+       ];
+
        const books = storedBooks;
 
        books.forEach((book) => UI.addBookToList(book));
