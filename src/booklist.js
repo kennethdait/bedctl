b60886736b4c871362320783510948cab3f3930f
Kenneth Dait <kendaitdev@icloud.com>
2021-02-19 17:09:25 -0600

    created method that adds hardcoded data to the DOM

--- src/booklist.js
+++ src/booklist.js
@@ -52,6 +52,21 @@
 
        books.forEach((book) => UI.addBookToList(book));
      }
+
+     static addBookToList(book) {
+       // create the `table` row, to insert into the book-list table
+       const list = document.querySelector('#book-list');
+       
+       const row = document.createElement('tr');
+       row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+      `;
+
+      list.appendChild(row);
+     }
    }
 
 
